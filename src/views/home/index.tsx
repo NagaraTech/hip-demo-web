b97b18c640a9a344d35ade1fbd38728a197519ad
rongbai <1577973940@qq.com>
2024-03-19 18:50:08 +0800

    add eth to bip private key demo on login page

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,9 +1,53 @@
+import {etc,getPublicKey}  from '@noble/secp256k1'
+import {hkdf} from '@noble/hashes/hkdf'
+import {sha256} from '@noble/hashes/sha256'
+import  { useState } from "react";
+
+
+
 
 function Home() {
+
+    // const { sdk } = useSDK();
+    const [signedMessage, setSignedMessage] = useState("1123123123");
+
+
+    const handleConnectAndSign = async () => {
+        try {
+            // const message = "Login Nostr";
+            // // @ts-ignore
+            // const signature = await sdk?.connectAndSign({ msg: message });
+            const signature = "d9a8843438c2a319dead72cb0b50a7959de881c63d1c99ee11065a9ebf7be6fd"
+            const nostr_privateKey = await  privateKeyFromX(signature)
+            console.log(nostr_privateKey)
+            console.log(etc.bytesToHex(getPublicKey(nostr_privateKey)))
+            setSignedMessage(nostr_privateKey);
+        } catch (error) {
+            console.error("Error in signing:", error);
+        }
+    };
+
+    async function privateKeyFromX(sig: string) {
+        const inputKey = await sha256(etc.hexToBytes(sig.toLowerCase().startsWith("0x") ? sig.slice(2) : sig))
+        const info = `${sig}`
+        const salt = await sha256(`${sig.slice(-64)}`)
+        const hashKey = await hkdf(sha256, inputKey,salt, info,42)
+        return etc.bytesToHex(etc.hashToPrivateKey(hashKey))
+    }
+
+
     return (
         <div className='home-container'>
             <div>首页</div>
+            <button
+                className={`bg-gray-500 rounded-full hover:bg-gray-700 text-white font-bold py-2 px-4 ml-8 
+                }`}
+                onClick={handleConnectAndSign}
+            >
+                {signedMessage}
+            </button>
         </div>
     )
 }
+
 export default Home
--- src/views/login/index.tsx
+++ src/views/login/index.tsx
@@ -2,12 +2,18 @@ import { Link, useNavigate } from 'react-router-dom';
 import { useSDK } from "@metamask/sdk-react";
 import hetuLogo from '@/assets/images/hetu-logo.png'
 import hetucn from '@/assets/images/hetu-cn.png'
+import {etc, getPublicKey} from "@noble/secp256k1";
+import {useState} from "react";
+import {sha256} from "@noble/hashes/sha256";
+import {hkdf} from "@noble/hashes/hkdf";
 
 const Login = () => {
 
     const navigate = useNavigate();
     const { sdk } = useSDK()
 
+    const [privateKey, setPrivateKey] = useState("1123123123");
+    const [publicKey, setPublicKey] = useState("1123123123");
     const connect = async () => {
         try {
             const accounts = await sdk?.connect();
@@ -17,6 +23,36 @@ const Login = () => {
             console.warn("failed to connect..", err);
         }
     };
+
+    const disConnect = async () => {
+       sdk?.terminate()
+    };
+
+
+    const handleConnectAndSign = async () => {
+        try {
+            const message = "Login Nostr";
+            // @ts-ignore
+            const signature = await sdk?.connectAndSign({ msg: message });
+            // const signature = "d9a8843438c2a319dead72cb0b50a7959de881c63d1c99ee11065a9ebf7be6fd"
+            const nostr_privateKey = await  privateKeyFromX(signature)
+            console.log(nostr_privateKey)
+            console.log(etc.bytesToHex(getPublicKey(nostr_privateKey)))
+            setPrivateKey(nostr_privateKey);
+            setPublicKey(etc.bytesToHex(getPublicKey(nostr_privateKey)));
+            sdk?.terminate();
+        } catch (error) {
+            console.error("Error in signing:", error);
+        }
+    };
+
+    async function privateKeyFromX(sig: string) {
+        const inputKey = await sha256(etc.hexToBytes(sig.toLowerCase().startsWith("0x") ? sig.slice(2) : sig))
+        const info = `${sig}`
+        const salt = await sha256(`${sig.slice(-64)}`)
+        const hashKey = await hkdf(sha256, inputKey,salt, info,42)
+        return etc.bytesToHex(etc.hashToPrivateKey(hashKey))
+    }
 
     return (
 
@@ -43,15 +79,27 @@ const Login = () => {
                                 alt="logo"
                             />
                             <h1 className="text-4xl font-bold text-center text-white">Welcome to Hetu Hips Playground</h1>
-                                <div className="flex flex-col items-center justify-center">
-                                    <button onClick={connect}
-                                            className="py-2 mt-20 mb-4 text-balance font-bold text-white rounded-lg w-56 bg-blue-600 hover:bg-blue-800">
-                                        Connect MetaMask Login
-                                    </button>
-                                    {/*<button onClick={disconnect}*/}
-                                    {/*        className="py-2 mt-20 mb-4 text-lg font-bold text-white rounded-lg w-56 bg-blue-600 hover:bg-blue-800">Disconnect*/}
-                                    {/*</button>*/}
-                                </div>
+                            <div className="flex flex-col items-center justify-center">
+                                <button onClick={connect}
+                                        className="py-2 mt-20 mb-4 text-balance font-bold text-white rounded-lg w-56 bg-blue-600 hover:bg-blue-800">
+                                    Connect MetaMask Login
+                                </button>
+
+                                <button onClick={disConnect}
+                                        className="py-2 mt-20 mb-4 text-balance font-bold text-white rounded-lg w-56 bg-blue-600 hover:bg-blue-800">
+                                    DisConnect MetaMask
+                                </button>
+                                {/*<button onClick={disconnect}*/}
+                                {/*        className="py-2 mt-20 mb-4 text-lg font-bold text-white rounded-lg w-56 bg-blue-600 hover:bg-blue-800">Disconnect*/}
+                                {/*</button>*/}
+
+                                <button onClick={handleConnectAndSign}
+                                        className="py-2 mt-20 mb-4 text-balance font-bold text-white rounded-lg w-56 bg-blue-600 hover:bg-blue-800">
+                                    generate bip private key
+                                </button>
+                                PrivateKey: <text>{privateKey} </text>
+                                PublicKey: <text>{publicKey}</text>
+                            </div>
                         </div>
                     </div>
                 </div>
